0ce0d045a6d4ce68cdf05fe5417754b236963283
Ruben Hernandez <rubendez21@gmail.com>
2026-02-03 15:46:08 -0600

    Se creó una rama para el desarrollo del tutorial del juego. Se añadió JSON del alfabeto en Braille. Se inició el desarrollo de la clase gameui, esta clase controlará toda la interfaz del juego en el futuro. se creó el módulo tutorial, el cual será llamado al elegir la opción Tutorial del menú principal.
    Signed-off-by: Ruben Hernandez <rubendez21@gmail.com>

---
+++ gameui.py
@@ -0,0 +1,34 @@
+import os
+import json
+import pygame
+from perfil import get_color
+from entorno_instalacion import base_path
+
+class GameUI:
+    def __init__(self):
+        self.user = None
+        self.screen = None
+        self.font = None
+
+    def ui_tutorial(self, screen, font):
+        options = ["Juego", "Tutorial", "Ranking", "Salir"]
+        selected = 0
+        last_selected = -1
+        last_tts_time = 0
+
+
+        ruta = os.path.join(base_path, "sys", "alfabeto.json")
+        with open(ruta, "r") as file:
+            alphabet = json.load(file)
+        letters = alphabet["BASE"]
+        for i, option in enumerate(letters):
+            color = get_color("CL_TEXTO_RES") if i == selected else get_color("CL_TEXTO")
+            self.draw_text(option, font, color, screen, 40, 100 + i * 50)
+
+        pygame.display.flip()
+
+    def draw_text(self, text, font, color, surface, x, y):
+        obj = font.render(text, True, color)
+        rect = obj.get_rect(topleft=(x, y))
+        surface.blit(obj, rect)
+    
--- tutorial.py
+++ tutorial.py
@@ -1,2 +1,9 @@
+import os
+import json
+from speaker import sp
+from entorno_instalacion import base_path
+
 def jugar_tutorial():
     pass
+
+jugar_tutorial()
