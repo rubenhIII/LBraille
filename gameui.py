aa4eb40f727b5a6a48bb78ad8922b4b981adc372
Ruben Hernandez <rubendez21@gmail.com>
2026-02-04 12:24:46 -0600

    Se modificó la clase gameui. Ya muestra el menu y reacciona a teclas arriba abajo. Este commit es desde la WORKSTATION
    Signed-off-by: Ruben Hernandez <rubendez21@gmail.com>

--- gameui.py
+++ gameui.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import pygame
 from perfil import get_color
@@ -15,17 +16,28 @@ class GameUI:
         selected = 0
         last_selected = -1
         last_tts_time = 0
-
-
         ruta = os.path.join(base_path, "sys", "alfabeto.json")
         with open(ruta, "r") as file:
             alphabet = json.load(file)
         letters = alphabet["BASE"]
-        for i, option in enumerate(letters):
-            color = get_color("CL_TEXTO_RES") if i == selected else get_color("CL_TEXTO")
-            self.draw_text(option, font, color, screen, 40, 100 + i * 50)
+        while True:
+            for i, option in enumerate(letters):
+                color = get_color("CL_TEXTO_RES") if i == selected else get_color("CL_TEXTO")
+                self.draw_text(option, font, color, screen, 40, 100 + i * 50)
+            pygame.display.flip()
 
-        pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key in [pygame.K_UP, pygame.K_BACKSPACE]:
+                        selected -= 1 % len(options)
+                    elif event.key in [pygame.K_DOWN, pygame.K_SPACE]:
+                        selected += 1 % len(options)
+                    elif event.key == pygame.K_q:
+                        pygame.quit()
+                        sys.exit()
 
     def draw_text(self, text, font, color, surface, x, y):
         obj = font.render(text, True, color)
---
+++ pruebas/test_tutorial_menu.py
@@ -0,0 +1,31 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from auth import login_o_registrar_usuario
+from menu import menu
+import pygame
+from perfil import cargar_configuracion_global
+from gameui import GameUI
+
+# Inicializar Pygame y pantalla
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Aprender a usar el teclado")
+
+# Fuente global
+font = pygame.font.Font(None, 40)
+
+# Obtener usuario desde login o registro
+usuario = "Ruben"
+
+# Cargar configuración inicial (para tamaños y colores)
+cargar_configuracion_global()
+
+# Iniciar menú
+gui = GameUI()
+gui.ui_tutorial(screen, font)
+menu(usuario, screen, font)
+
+# Cerrar Pygame al salir
+pygame.quit()
